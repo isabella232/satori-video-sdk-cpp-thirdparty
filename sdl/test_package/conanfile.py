79ecf51985d3a4c6610063c5d6058737e2b42522
Mykhailo Aizatskyi <maizatskyi@mz.com>
2017-09-01 20:11:16 +0000

    adding test options

--- sdl/test_package/conanfile.py
+++ sdl/test_package/conanfile.py
@@ -4,10 +4,11 @@ import os
 class SdlTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+    options = {"fPIC": [True, False], "shared": [True, False]}
+    default_options = "fPIC=False", "shared=False"
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
         cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
         cmake.build()
 
