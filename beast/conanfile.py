3d731e69393fc9b3e6064e490abd41facdb511eb
Mykhailo Aizatskyi <maizatskyi@mz.com>
2017-09-21 21:45:54 +0000

    new beast version

--- beast/conanfile.py
+++ beast/conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile
 
 class BeastConan(ConanFile):
     name = "Beast"
-    version = "95"
-    tag = "e3c79edebd94c1ac029466e4b172a6a9f04ba252"
+    version = "117"
+    tag = "0f5ea371c18a6797f4623a61f159326428c01400"
     license = "MIT"
     url = "https://github.com/boostorg/beast"
     requires = "Boost/1.64.0@satorivideo/master"
