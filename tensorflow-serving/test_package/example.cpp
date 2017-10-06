041050b3b45d5689ff19b2463eac0666524a9e3b
Renat Idrisov <ridrisov@mz.com>
2017-10-06 14:41:14 +0200

    Better example for TF

--- tensorflow-serving/test_package/example.cpp
+++ tensorflow-serving/test_package/example.cpp
@@ -1,5 +1,17 @@
 #include <tensorflow/core/framework/tensor.h>
 #include <tensorflow/core/platform/types.h>
+#include <tensorflow/core/public/session.h>
+
+#include <tensorflow/cc/saved_model/loader.h>
+#include <tensorflow/cc/saved_model/signature_constants.h>
+#include <tensorflow/cc/saved_model/tag_constants.h>
+
+void load_model() {
+  tensorflow::SavedModelBundle bundle;
+  tensorflow::Status load_model_status = tensorflow::LoadSavedModel(
+      tensorflow::SessionOptions(), tensorflow::RunOptions(), "fake_model_dir",
+      {tensorflow::kSavedModelTagTrain}, &bundle);
+}
 
 int main() {
   tensorflow::string str;
