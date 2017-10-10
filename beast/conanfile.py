525a15a570007b1172ff74f89a4fa6bfa1f62b38
Mykhailo Aizatskyi <maizatskyi@mz.com>
2017-10-10 18:37:25 +0000

    122_experimental version

--- beast/conanfile.py
+++ beast/conanfile.py
@@ -3,14 +3,17 @@ from conans import ConanFile
 
 class BeastConan(ConanFile):
     name = "Beast"
-    version = "120"
-    tag = "7fe74b1bf544a64ecf8985fde44abf88f9902251"
+    
+    version = "122_experimental"
+    tag = "f09b2d3e1c9d383e5d0f57b1bf889568cf27c39f"
+    git_url = "https://github.com/vinniefalco/beast.git"
+
     license = "MIT"
     url = "https://github.com/boostorg/beast"
     requires = "Boost/1.64.0@satorivideo/master"
 
     def source(self):
-        self.run("git clone https://github.com/boostorg/beast")
+        self.run("git clone %s " % self.git_url)
         self.run("cd beast && git checkout %s" % self.tag)
 
     def package(self):
