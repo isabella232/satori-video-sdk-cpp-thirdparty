3cdd9536071ea024438b67cbca0d8550ce74748a
Oleg Khabinov <oleg.khabinov@gmail.com>
2017-10-12 12:41:29 -0700

    Use v1.5.0 loguru (#1)

--- loguru/conanfile.py
+++ loguru/conanfile.py
@@ -3,15 +3,13 @@ from conans import ConanFile
 
 class LoguruConan(ConanFile):
     name = "Loguru"
-    version = "1.5.1"
-    tag = "75606d74bd1d374e8525a53db91f054b8b7d4cac"
+    version = "1.5.0"
     license = "custom"
     url = "https://github.com/emilk/loguru"
     # No settings/options are necessary, this is header only
 
     def source(self):
-        self.run("git clone --depth 1 https://github.com/emilk/loguru")
-        self.run("cd loguru && git checkout %s" % self.tag)
+        self.run("git clone -b v%s --depth 1 https://github.com/emilk/loguru" % self.version)
 
     def package(self):
         self.copy("*.hpp", "include")
