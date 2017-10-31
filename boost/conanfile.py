42caea8f299daef1a44080e97b607438c5e73e42
Mike Aizatsky <mike.aizatsky@gmail.com>
2017-10-30 18:49:57 -0700

    version bump

--- boost/conanfile.py
+++ boost/conanfile.py
@@ -5,7 +5,7 @@ import sys
 
 class BoostConan(ConanFile):
     name = "Boost"
-    version = "1.65.1_03"
+    version = "1.65.1_04"
     tag = "1.65.1"
     license = "Boost Software License"
     url = "http://www.boost.org/"
